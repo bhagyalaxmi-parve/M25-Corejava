08d42a6258a00c1ee7f1de4c3f4f0a4c8c6dc821
user <user@DESKTOP-CHB38O4>
2022-09-26 18:50:54 +0530

    second commit

--- Day-1_ConnectingEclipseWithGitHub/src/org/tnsindia/basic/TestingEclipseForGitHub.java
+++ Day-1_ConnectingEclipseWithGitHub/src/org/tnsindia/basic/TestingEclipseForGitHub.java
@@ -1,11 +1,14 @@
 package org.tnsindia.basic;
 
+
+//program on to print the statement..........Single lie comment
 public class TestingEclipseForGitHub {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Welcome to TNS India Foundation");
+		
 
 	}
 
