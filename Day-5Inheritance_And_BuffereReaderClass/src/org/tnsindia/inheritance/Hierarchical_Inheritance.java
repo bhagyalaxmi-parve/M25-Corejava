266ba7ed3ec6496a2353c1ba37a6268a84582123
user <user@DESKTOP-CHB38O4>
2022-10-11 12:15:26 +0530

    first commit

---
+++ Day-5Inheritance_And_BuffereReaderClass/src/org/tnsindia/inheritance/Hierarchical_Inheritance.java
@@ -0,0 +1,51 @@
+package org.tnsindia.inheritance;
+
+
+
+//Parent class
+class AndroidVersion
+{
+	protected String name="Android latest version";
+	
+	public void display1()
+	{
+		System.out.println(name);
+	}
+	
+}
+//child class 1
+class Tiramisu extends AndroidVersion
+{
+	public String v13="Tiramisu";
+	//method overriding
+	public void display()
+	{
+		System.out.println(v13);
+	}
+}
+//child class 2
+class SnowCone extends AndroidVersion
+{
+	public String v12="Snow Cone";
+	//method overriding
+	public void display()
+	{
+		System.out.println(v12);
+	}
+}
+
+public class Hierarchical_Inheritance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Tiramisu t=new Tiramisu();
+		t.display();
+		SnowCone s=new SnowCone();
+		s.display();
+		t.display1();
+		
+	}
+
+}
