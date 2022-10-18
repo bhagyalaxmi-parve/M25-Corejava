fe4303ba72db80784d7bd84ab8743a579015a1cc
user <user@DESKTOP-CHB38O4>
2022-10-18 18:36:44 +0530

    second commit

--- Assignment-1_Operators/src/org/tnsindia/operator/Bitwise_Operator.java
+++ Assignment-1_Operators/src/org/tnsindia/operator/Bitwise_Operator.java
@@ -1,60 +1,21 @@
 package org.tnsindia.operator;
 
-//public class Bitwise_Operator {
-//
-//	public static void main(String[] args) {
-//		// TODO Auto-generated method stub
-//		
-//		int a=8 ,b=2;
-//		int res1 = a & b;
-//		int res2 = a | b;
-//		int res3 = a ^ b;
-//		System.out.println(res1);
-//		System.out.println(res2);
-//		System.out.println(res3);
-//
-//
-//	}
-//
-//}
+public class Bitwise_Operator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=8 ,b=2;
+		int res1 = a & b;
+		int res2 = a | b;
+		int res3 = a ^ b;
+		System.out.println(res1);
+		System.out.println(res2);
+		System.out.println(res3);
 
 
-class Test
-{
-
-static int a;
-
-static
-
-{ 
-	a=4;
-
-System.out.println ("inside static blockn"); 
-System.out.println("a="+a);
+	}
 
 }
 
-Test()
-
-
-{ 
-	System.out.println ("ninside constructorn");
-	a = 10;
-}
-
-public static void func() {
-
-a=a+1;
-
-
-System.out.println("a = "+a);
-}
-
-@SuppressWarnings("static-access")
-public static void main(String[] args)
-{
-
-Test obj = new Test(); obj.func();
-}
-}
 
--- Day-10Exception_Handling/src/org/tnsindia/exception/Throw_Keyword.java
+++ Day-10Exception_Handling/src/org/tnsindia/exception/Throw_Keyword.java
@@ -1,54 +1,32 @@
 package org.tnsindia.exception;
-//
-//public class Throw_Keyword {
-//
-//	
-//	public static void display(int age, int weight)
-//	{
-//		
-//		if(age>18  && weight>45)
-//		{
-//			System.out.println("Eligible to donote blood");
-//	}
-//		else
-//		{
-//			throw new ArithmeticException("not eligible");
-//		}
-//	
-//	}
-//	public static void main(String[] args) {
-//		// TODO Auto-generated method stub
-//		
-//		try {
-//			display(20, 30);
-//		}
-//		catch(ArithmeticException  e)
-//		{
-//			System.out.println(e);
 
+public class Throw_Keyword {
 
-
-//		}
-
-
-class Main {
-
-public static void main(String args[]) {
-
-try {
-
-throw 10;
-
-}
-
-catch(int e) {
-
-System.out.println("Got the Exception " + e);
-
-}
-
-}
+	
+	public static void display(int age, int weight)
+	{
+		
+		if(age>18  && weight>45)
+		{
+			System.out.println("Eligible to donote blood");
+	}
+		else
+		{
+			throw new ArithmeticException("not eligible");
+		}
+	
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		try {
+			display(20, 30);
+		}
+		catch(ArithmeticException  e)
+		{
+			System.out.println(e);
+		}
 
 	}
 
-
+}
